c9ab822ca52c30deda2077e4ecd04a4f8bb4fc9e
Kalandor01 <rohovszkyakoska@gmail.com>
2022-04-27 12:57:05 +0200

    tesztek

--- js/tesztek.js
+++ js/tesztek.js
@@ -15,6 +15,7 @@ function query_all(name, arg) {
     document.querySelectorAll(name).forEach(elem => {arg(elem)});
 }
 
+const before_lehetoseg = "-";
 var sheets = [];
 var right = 0;
 var guesses = 0;
@@ -22,6 +23,7 @@ var guesses = 0;
 function init()
 {
     process_xlsx();
+    query_all("#test_alt>h3, #test_koz>h3, #test_fel>h3", q=>q.onclick = click_test);
 }
 
 function process_xlsx()
@@ -62,65 +64,101 @@ function make_tests()
 {
     //sheets processing
     let sh_processed = [];
-    for (let x = 0; x < sheets.length; x++)
+    for (let x = 1; x < sheets.length; x++)
     {
         let sh_temp = [];
         let val_temp = [];
-        let ker_str = [sheets[1][0].Kérdések, sheets[1][0].Válaszok];
-        val_temp.push(sheets[1][0].Válaszok.replace("Helyes válasz: ", " - "));
-        for (let x = 1; x < sheets[1].length; x++)
+        let lehet_temp = [];
+        let ker_str = [sheets[x][0].Kérdések];
+        if(sheets[x][0].Válaszok.includes("Helyes válasz: "))
         {
-            if(sheets[1][x].Kérdések != undefined)
+            val_temp.push(sheets[x][0].Válaszok.replace("Helyes válasz: ", ""));
+            lehet_temp.push(sheets[x][0].Válaszok.replace("Helyes válasz: ", before_lehetoseg));
+        }
+        else
+            lehet_temp.push(sheets[x][0].Válaszok.replace(" - ", before_lehetoseg));
+        for (let y = 1; y < sheets[x].length; y++)
+        {
+            if(sheets[x][y].Kérdések != undefined)
             {
-                sh_temp.push([ker_str, val_temp]);
-                ker_str = [sheets[1][x].Kérdések, sheets[1][x].Válaszok]
+                sh_temp.push([ker_str, val_temp, lehet_temp]);
+                ker_str = [sheets[x][y].Kérdések]
                 val_temp = [];
-                val_temp.push(sheets[1][x].Válaszok.replace("Helyes válasz: ", " - "));
+                lehet_temp = [];
+                val_temp.push(sheets[x][y].Válaszok.replace("Helyes válasz: ", ""));
+                lehet_temp.push(sheets[x][y].Válaszok.replace("Helyes válasz: ", before_lehetoseg));
             }
             else
-                val_temp.push(sheets[1][x].Válaszok);
+            {
+                if(sheets[x][y].Válaszok.includes("Helyes válasz: "))
+                {
+                    val_temp.push(sheets[x][y].Válaszok.replace("Helyes válasz: ", ""));
+                    lehet_temp.push(sheets[x][y].Válaszok.replace("Helyes válasz: ", before_lehetoseg));
+                }
+                else
+                    lehet_temp.push(sheets[x][y].Válaszok.replace(" - ", before_lehetoseg));
+            }
         }
+        sh_temp.push([ker_str, val_temp, lehet_temp]);
         sh_processed.push(sh_temp);
     }
     //write out
     console.log(sh_processed);
-    sh_processed[0].forEach(kerdes => {
-        let kerdes_div = '<div class="kerdes">';
-        kerdes_div += `<h4>${kerdes[0][0]}</h4><div class="valaszok"><h5>${kerdes[0][1]}</h5>`;
-        kerdes[1].forEach(valasz => {
-            kerdes_div += `<h6>${valasz}</h6>`;
-        });
-        kerdes_div += "</div></div>";
-        query("#test_alt>.tests").innerHTML += kerdes_div;
-    });
-    sh_processed[0].forEach(kerdes => {
-        let kerdes_div = '<div class="kerdes">';
-        kerdes_div += `<h4>${kerdes[0][0]}</h4><div class="valaszok"><h5>${kerdes[0][1]}</h5>`;
-        kerdes[1].forEach(valasz => {
-            kerdes_div += `<h6>${valasz}</h6>`;
-        });
-        kerdes_div += "</div></div>";
-        query("#test_koz>.tests").innerHTML += kerdes_div;
-    });
-    sh_processed[0].forEach(kerdes => {
-        let kerdes_div = '<div class="kerdes">';
-        kerdes_div += `<h4>${kerdes[0][0]}</h4><div class="valaszok"><h5>${kerdes[0][1]}</h5>`;
-        kerdes[1].forEach(valasz => {
-            kerdes_div += `<h6>${valasz}</h6>`;
-        });
-        kerdes_div += "</div></div>";
-        query("#test_fel>.tests").innerHTML += kerdes_div;
-    });
+    write_test_help(sh_processed[0], "alt");
+    write_test_help(sh_processed[1], "koz");
+    write_test_help(sh_processed[2], "fel");
 
-    //onclick?
-    query_all(".kerdes>.valaszok>h6", q=>q.onclick = click_valasz);
+    //onclick
+    query_all(".kerdes>.lehetosegek>h6", q=>q.onclick = click_valasz);
 }
 
-//ROSZ MEGOLDÁS
+
+//DISPLAY NONE FOR ALL TEST DIVS EXEPT THE CLICKED + RESET SCORE ON CLICK
+function click_test(evt)
+{
+    let clicked_num = Array.from(evt.target.parentElement.parentElement.children).indexOf(evt.target.parentElement)
+    console.log(clicked_num);
+    console.log(evt.target.parentElement.parentElement.children);
+    Array.from(evt.target.parentElement.parentElement.children).forEach(test => {
+        test.children.style.display = "none";
+    });
+}
+
+function write_test_help(cur_sheet, name)
+{
+    cur_sheet.forEach(kerdes => {
+        let kerdes_div = '<div class="kerdes">';
+        kerdes_div += `<h4>${kerdes[0]}</h4><div class="valaszok">`;
+        kerdes[1].forEach(valasz => {
+            kerdes_div += `<p>${valasz}</p>`;
+        });
+        //lehetőségek
+        kerdes_div += `</div><div class="lehetosegek">`;
+        let lehetosegek = kerdes[2].copyWithin();
+        while(lehetosegek.length > 0)
+        {
+            let r = Math.floor(Math.random() * (lehetosegek.length))
+            kerdes_div += `<h6>${lehetosegek[r]}</h6>`;
+            lehetosegek.splice(r, 1);
+        }
+        kerdes_div += "</div></div>";
+        query(`#test_${name}>.tests`).innerHTML += kerdes_div;
+    });
+}
+
+function check_ans(lehet)
+{
+    let van = false;
+    Array.from(lehet.parentElement.parentElement.children[1].children).forEach(answer => {
+        if(answer.innerHTML.replace(" ", "") == lehet.innerHTML.replace(before_lehetoseg, "").replace(" ", ""))
+            van = true;
+    });
+    return van;
+}
+
 function click_valasz(evt)
 {
-    console.log(evt.target.parentElement);
-    if(evt.target.innerHTML.replace(" - ", "") == evt.target.parentElement.children[0].innerHTML.replace("Helyes válasz: ", ""))
+    if(check_ans(evt.target))
     {
         evt.target.parentElement.innerHTML = "Helyes!";
         right++;
