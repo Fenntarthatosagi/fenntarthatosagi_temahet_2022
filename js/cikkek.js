cccf4b433a53380ca6fea5732b227d2b65fe7c0d
Blaj3n <ulrichbence15@freemail.hu>
2022-05-04 12:46:36 +0200

    Gréti cikk alap elkészítése

--- js/cikkek.js
+++ js/cikkek.js
@@ -1,14 +1,14 @@
 // foCim: <h1>, kep: <img>, bekezdes: <p>, alCim: <h3>
-var cikkek = [
+var GretiCikk = [
   {
     foCim: "Mi az a TeSZedd!?",
     kep: "img/cikkek/kep1.jpg",
     bekezdes:
-      "A TeSzedd! Önkéntesen a tiszta Magyarországért akció. Hazánk legnagyobb önkéntes mozgalma. Idén immár kilencedik alkalommal valósul meg. A szemétgyűjtési akció keretében szerte az országban „nagytakarítanak” a TeSzedd! önkéntesei. Azért szervezzük meg ezt a mozgalmat minden évben, hogy közösen megtisztítsuk szűkebb-tágabb környezetünket.",
+      "A TeSzedd! Önkéntesen a tiszta Magyarországért akció. Hazánk legnagyobb önkéntes mozgalma.Idén immár kilencedik alkalommal valósul meg. A szemétgyűjtési akció keretében szerte az országban „nagytakarítanak” a TeSzedd! önkéntesei. Azért szervezzük meg ezt a mozgalmat minden évben, hogy közösen megtisztítsuk szűkebb-tágabb környezetünket.",
   },
   {
     alCim: "Vízcsepp összművészeti pályázat 2022",
-    kep: "img/cikkek/kep2",
+    kep: "img/cikkek/kep2.jpg",
     bekezdes:
       "A pályázat a 2022. évi Fenntarthatósági Témahét keretében kerül meghirdetésre. A pályázat célja a diákok szemléletformálása vízhez kapcsolódó művészeti alkotások készítésével. Benyújtható pályázatok: rajz, fotó, szépirodalmi mű, zenemű, képregény, infografika, kisfilm.",
     bekezdes:
@@ -16,7 +16,7 @@ var cikkek = [
   },
   {
     alCim: "Kert-kóstolgató pályázat",
-    kep: "img/cikkek/kep3",
+    kep: "img/cikkek/kep3.jpg",
     bekezdes:
       "A pályázat célja, hogy az iskolás csoportok egy saját tervezésű legalább 1 m2 iskolakertet hozzanak létre. Az iskolakertbe olyan növények ültetését várjuk, amelyek az a fenntarthatóságot szolgálják. Az iskolakert létrehozását dokumentálni kell a tervezéstől a kivitelezésig",
   },
@@ -35,17 +35,25 @@ function $2(elem) {
 
 function init() {
   var txt = "";
-  for (let index = 0; index < cikkek.length; index++) {
-    txt +=
-      "<div><h1>" + cikkek[index].foCim + "</h1>" +
-      "<img src=" + cikkek[index].kep + "></img>" +
-      "<p>" + cikkek[index].bekezdes + "</p>" +
-      "<h3>" + cikkek[index].alCim + "</h3>" +
-      "</div>";
+  for (let index = 0; index < GretiCikk.length; index++) {
+    txt += "<div>";
+    for (const key in GretiCikk[index]) {
+      if (key == "foCim") {
+        txt += "<h1>" + GretiCikk[index][key] + "</h1>";
+      }
+      if (key == "kep") {
+        txt += "<img src=" + GretiCikk[index][key] + "></img>";
+      }
+      if (key == "bekezdes") {
+        txt += "<p>" + GretiCikk[index][key] + "</p>";
+      }
+      if (key == "alCim") {
+        txt += "<h3>" + GretiCikk[index][key] + "</h3>";
+      }
+    }
+    txt += "</div>";
   }
 
-  
-
-  $1("#m").innerHTML = txt;
-  console.log()
+  $1("#main").innerHTML = txt;
+  console.log();
 }
