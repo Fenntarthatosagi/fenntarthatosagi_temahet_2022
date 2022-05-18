3aeb9591d90a1a2e4a9d29d3b32fd0e91d6824d0
Jegesmaci583 <hunraindow@gmail.com>
2022-05-18 08:47:16 +0200

    Main szoveg részek

--- main.js
+++ main.js
@@ -1,50 +1,88 @@
 window.addEventListener("load", init);
 
-function query(name) {
+function querry(name) {
     return document.querySelector(name);
 }
 
-function query_all_raw(name) {
+function querry_all_raw(name) {
     return document.querySelectorAll(name);
 }
 
-function query_all(name, arg) {
-    document.querySelectorAll(name).forEach(elem => {arg(elem)});
+function querry_all(name, arg) {
+    document.querySelectorAll(name).forEach(elem => { arg(elem) });
 }
 
-function init()
-{
+function init() {
     //load main elements
-    query("header").innerHTML += '<div id="theme"><button class="theme_button" onclick="setTheme(false)"><img src="img/sun.png" alt="Light mode"></button></div>';
-    query("nav").innerHTML += '<ul><li><a href="index.html">Kezdőoldal</a></li><li><a href="cikkek.html">Cikkek</a></li><li><a href="tesztek.html">Tesztek</a></li><li><a href="galeria/bemutato.html">Ruha bemutató</a></li><li><a href="jatekok.html">Játékok</a></li></ul>';
-    query("footer").innerHTML += '<p>teszt</p>';
+    querry("header").innerHTML += '<div id="theme"><button class="theme_button" onclick="setTheme(false)"><img src="img/sun.png" alt="Light mode"></button></div>';
+    querry("nav").innerHTML += '<ul><li><a href="index.html">Kezdőoldal</a></li><li><a href="cikkek.html">Cikkek</a></li><li><a href="tesztek.html">Tesztek</a></li><li><a href="ruha_bemutato.html">Ruha bemutató</a></li><li><a href="jatekok.html">Játékok</a></li></ul>';
+    querry("body").innerHTML += '';
+    querry("article").innerHTML += '<img src="">';
+    querry("aside").innerHTML += '';
+    querry("footer").innerHTML += '<p>Minden jog fenntartva ©</p>';
     //document.cookie = "theme=; expires=Thu, 01 Jan 1970 00:00:00 UTC";
+    fetch("ajanlo.json")
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      console.log(data.cikkek);
+      data.cikkek.forEach((elem) => {
+        cikkekTomb.push(elem);
+      });
+      console.log(cikkekTomb);
+      feldolgoz();
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 }
+
+const cikkekTomb = []
+
+function feldolgoz() {
+    var txt = "";
+    cikkekTomb.forEach(function (cikk) {
+      txt += "<div>";
+      for (const key in cikk) {
+        console.log(key);
+        if (key == "cnev") {
+          txt += "<h1>" + cikk[key] + "</h1>";
+        }
+        if (key == "ckep") {
+          txt += "<img src=" + cikk[key] + "></img>";
+        }
+        if (key.indexOf("leiras") >= 0) {
+            txt += "<p>" + cikk[key] + "</p>";
+        }
+      }
+      txt += "</div>";
+    });
+    console.log(txt);
+    querry("article").innerHTML = txt;
+  }
 
 
 //set theme
 var theme = "dark";
 
-function setTheme(set_cookie=true){
+function setTheme(set_cookie = true) {
     //empty
-    query("#theme>.theme_button>img").remove();
+    querry("#theme>.theme_button>img").remove();
 
-    if(theme=="dark")
-    {
+    if (theme == "dark") {
         //replace dark with light
         //theme
-        query("#theme>.theme_button").innerHTML = `<img src="img/moon.png" alt="Dark mode">`;
+        querry("#theme>.theme_button").innerHTML = `<img src="img/moon.png" alt="Dark mode">`;
         theme = "light";
-        if(set_cookie)
+        if (set_cookie)
             document.cookie = "theme=1; expires=Tue, 1 Jan 2030 12:00:00 UTC; SameSite=Strict";
     }
-    else
-    {
+    else {
         //replace light with dark
         //theme
-        query("#theme>.theme_button").innerHTML = `<img src="img/sun.png" alt="Light mode">`;
+        querry("#theme>.theme_button").innerHTML = `<img src="img/sun.png" alt="Light mode">`;
         theme = "dark";
-        if(set_cookie)
+        if (set_cookie)
             document.cookie = "theme=0; expires=Tue, 1 Jan 2030 12:00:00 UTC; SameSite=Strict";
     }
     //set html class for color
