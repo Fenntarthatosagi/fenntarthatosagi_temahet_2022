45edd4065c8ad7b83e560247f22ca037a8864c47
Kalandor01 <54396021+Kalandor01@users.noreply.github.com>
2022-05-20 16:37:22 +0200

    navbar in js remove theme change

--- main.js
+++ main.js
@@ -14,13 +14,11 @@ function querry_all(name, arg) {
 
 function init() {
     //load main elements
-    querry("header").innerHTML += '<div id="theme"><button class="theme_button" onclick="setTheme(false)"><img src="img/sun.png" alt="Light mode"></button></div>';
     querry("nav").innerHTML += '<ul><li><a href="index.html">Kezdőoldal</a></li><li><a href="cikkek.html">Cikkek</a></li><li><a href="tesztek.html">Tesztek</a></li><li><a href="galeria/galeria.html">Ruha bemutató</a></li><li><a href="jatekok.html">Játékok</a></li></ul>';
     querry("body").innerHTML += '';
     querry("article").innerHTML += '<img src="">';
     querry("aside").innerHTML += '';
     querry("footer").innerHTML += '<p>Minden jog fenntartva ©</p>';
-    //document.cookie = "theme=; expires=Thu, 01 Jan 1970 00:00:00 UTC";
     fetch("ajanlo.json")
     .then((response) => response.json())
     .then((data) => {
@@ -60,31 +58,3 @@ function feldolgoz() {
     console.log(txt);
     querry("article").innerHTML = txt;
   }
-
-
-//set theme
-var theme = "dark";
-
-function setTheme(set_cookie = true) {
-    //empty
-    querry("#theme>.theme_button>img").remove();
-
-    if (theme == "dark") {
-        //replace dark with light
-        //theme
-        querry("#theme>.theme_button").innerHTML = `<img src="img/moon.png" alt="Dark mode">`;
-        theme = "light";
-        if (set_cookie)
-            document.cookie = "theme=1; expires=Tue, 1 Jan 2030 12:00:00 UTC; SameSite=Strict";
-    }
-    else {
-        //replace light with dark
-        //theme
-        querry("#theme>.theme_button").innerHTML = `<img src="img/sun.png" alt="Light mode">`;
-        theme = "dark";
-        if (set_cookie)
-            document.cookie = "theme=0; expires=Tue, 1 Jan 2030 12:00:00 UTC; SameSite=Strict";
-    }
-    //set html class for color
-    document.documentElement.className = theme;
-}
--- navbar.js
+++ navbar.js
@@ -1,6 +1,70 @@
+window.addEventListener("load", init);
+
+function query(name) {
+    return document.querySelector(name);
+}
+
 const menuIconButton = document.querySelector("[data-menu-icon-btn]")
 const sidebar = document.querySelector("[data-sidebar]")
 
-menuIconButton.addEventListener("click", () => {
-    sidebar.classList.toggle("open")
-})
+function init()
+{
+    //menu button
+    menuIconButton.addEventListener("click", () => {
+        sidebar.classList.toggle("open")
+    })
+
+
+    query("nav").innerHTML = `
+    <!-- kezdőlap -->
+    <div class="top-sidebar">
+        <a href="/index.html" class="channel-logo"><img src="https://img.icons8.com/glyph-neue/344/home.png" alt="Channel Logo" /></a>
+        <div class="hidden-sidebar your-channel">Kezdőlap</div>
+        <div class="hidden-sidebar channel-name">Fenntarthatósági témahét</div>
+    </div>
+    <div class="middle-sidebar">
+        <ul class="sidebar-list">
+            <!-- cikkek -->
+            <li class="sidebar-list-item sidebar-cikkek">
+                <a href="/cikkek.html" class="sidebar-link">
+                    <img src="https://img.icons8.com/ios-filled/344/news.png" />
+                    <div class="hidden-sidebar">Cikkek</div>
+                </a>
+            </li>
+            <!-- tesztek -->
+            <li class="sidebar-list-item sidebar-tesztek">
+                <a href="/tesztek.html" class="sidebar-link">
+                    <img src="https://img.icons8.com/ios-filled/344/survey.png" />
+                    <div class="hidden-sidebar">Tesztek</div>
+                </a>
+            </li>
+            <!-- ruha bemutató -->
+            <li class="sidebar-list-item sidebar-galeria">
+                <a href="/galeria/galeria.html" class="sidebar-link">
+                    <img src="https://img.icons8.com/ios-filled/344/dress-front-view.png" />
+                    <div class="hidden-sidebar">Ruha bemutató</div>
+                </a>
+            </li>
+            <!-- játékok -->
+            <li class="sidebar-list-item sidebar-jatekok">
+                <a href="/jatekok.html" class="sidebar-link">
+                    <img src="https://img.icons8.com/ios-filled/344/controller.png" />
+                    <div class="hidden-sidebar">Játékok</div>
+                </a>
+            </li>
+        </ul>
+    </div>`;
+
+    if (document.URL.includes("cikkek.html")) {
+        query(".sidebar-cikkek").classList.toggle("active-page");
+    }
+    else if (document.URL.includes("tesztek.html")) {
+        query(".sidebar-tesztek").classList.toggle("active-page");
+    }
+    else if (document.URL.includes("galeria")) {
+        query(".sidebar-galeria").classList.toggle("active-page");
+    }
+    else if (document.URL.includes("jatekok.html")) {
+        query(".sidebar-jatekok").classList.toggle("active-page");
+    }
+}
