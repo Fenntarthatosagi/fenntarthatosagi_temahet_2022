dd7353c9dbd4006a1a4f8607a4703bd808d2e995
Kalandor01 <54396021+Kalandor01@users.noreply.github.com>
2022-05-20 19:01:30 +0200

    github mode test 1

--- navbar.js
+++ navbar.js
@@ -7,6 +7,9 @@ function query(name) {
 const menuIconButton = document.querySelector("[data-menu-icon-btn]")
 const sidebar = document.querySelector("[data-sidebar]")
 
+var github_mode = false;
+var github_link = "fenntarthatosagi_temahet_2022/";
+
 function init()
 {
     //menu button
@@ -14,11 +17,28 @@ function init()
         sidebar.classList.toggle("open")
     })
 
+    fetch("/galeria")
+    .then((response) => response.text())
+    .then((data) => {
+        github_mode = false;
+        console.log(data);
+        make_nav();
+    })
+    .catch((err) => {
+        github_mode = true;
+        console.log(err);
+        make_nav();
+    });
+}
 
+
+function make_nav()
+{
+    console.log(github_mode);
     query("nav").innerHTML = `
     <!-- kezdőlap -->
     <div class="top-sidebar">
-        <a href="/fenntarthatosagi_temahet_2022/index.html" class="channel-logo"><img src="https://img.icons8.com/glyph-neue/344/home.png" alt="Channel Logo" /></a>
+        <a href="/${github_mode ? github_link : ""}index.html" class="channel-logo"><img src="https://img.icons8.com/glyph-neue/344/home.png" alt="Channel Logo" /></a>
         <div class="hidden-sidebar your-channel">Kezdőlap</div>
         <div class="hidden-sidebar channel-name">Fenntarthatósági témahét</div>
     </div>
@@ -26,28 +46,28 @@ function init()
         <ul class="sidebar-list">
             <!-- cikkek -->
             <li class="sidebar-list-item sidebar-cikkek">
-                <a href="/fenntarthatosagi_temahet_2022/cikkek.html" class="sidebar-link">
+                <a href="/${github_mode ? github_link : ""}cikkek.html" class="sidebar-link">
                     <img src="https://img.icons8.com/ios-filled/344/news.png" />
                     <div class="hidden-sidebar">Cikkek</div>
                 </a>
             </li>
             <!-- tesztek -->
             <li class="sidebar-list-item sidebar-tesztek">
-                <a href="/fenntarthatosagi_temahet_2022/tesztek.html" class="sidebar-link">
+                <a href="/${github_mode ? github_link : ""}tesztek.html" class="sidebar-link">
                     <img src="https://img.icons8.com/ios-filled/344/survey.png" />
                     <div class="hidden-sidebar">Tesztek</div>
                 </a>
             </li>
             <!-- ruha bemutató -->
             <li class="sidebar-list-item sidebar-galeria">
-                <a href="/fenntarthatosagi_temahet_2022/galeria/galeria.html" class="sidebar-link">
+                <a href="/${github_mode ? github_link : ""}galeria/galeria.html" class="sidebar-link">
                     <img src="https://img.icons8.com/ios-filled/344/dress-front-view.png" />
                     <div class="hidden-sidebar">Ruha bemutató</div>
                 </a>
             </li>
             <!-- játékok -->
             <li class="sidebar-list-item sidebar-jatekok">
-                <a href="/fenntarthatosagi_temahet_2022/jatekok.html" class="sidebar-link">
+                <a href="/${github_mode ? github_link : ""}jatekok.html" class="sidebar-link">
                     <img src="https://img.icons8.com/ios-filled/344/controller.png" />
                     <div class="hidden-sidebar">Játékok</div>
                 </a>
